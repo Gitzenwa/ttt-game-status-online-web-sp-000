c20d48dcfba23a814bfbcf6a9ddf06e1bd5d05b7
Ezenwa Onukwue <eonukwue@gmail.com>
2020-07-18 22:56:06 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,24 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,7,8], # Bottom Row
+  [0,3,6], # Left columns
+  [1,4,7], # Middle column
+  [2,5,8], # Right coulum
+  [0,4,8], # left diagonal
+  [2,4,6], # right diagonal
+]
+def won?(board)
+  win_combination =[]
+  WIN_COMBINATIONS.each{|w_index| win_combination << w_index}
+  #returns falsey for an empty board, takes in board as an arguemnt, uses all  evaluates using all to see if the position is taken
+  #if board.all?{|index| position_taken?(board, index.to_i)}
+  #returns falsey for a draw, determine all the positions are taken, iteraate over the values, search the WIN_COMBINATIONS array for
+#elsif board.none?{|index| }
+  #return false if no win combinations
+  #return winning combination as an array
+end
